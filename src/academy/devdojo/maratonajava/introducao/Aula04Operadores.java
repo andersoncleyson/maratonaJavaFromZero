a6da7eaf17bd097e5c303b6b3e1c3736a68d6f5f
unknown <andersoncleyson@protonmail.com>
2024-10-28 11:50:25 -0300

    Logical operators

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -21,7 +21,42 @@ public class Aula04Operadores {
         System.out.println(rest);
 
         // < > <= >= == !=
-        boolean isDezMaiorQueVint = 10 > 20;
+        boolean isDezMaiorQueVint = 128 == 128;
         System.out.println(isDezMaiorQueVint);
+
+        // && (AND) || (OR) ! (logical not)
+
+        int age = 34;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = age > 30 && salario > 4621;
+        boolean isDentroDaLeiMenorQueTrinta = age < 30 && salario > 3381;
+
+        System.out.println("isDemtrpDaLeiMaiorQueTrinta: " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDemtrpDaLeiMenorQueTrinta:" + isDentroDaLeiMenorQueTrinta);
+
+        double valueTotalCountBank = 3000;
+        double valorTotalContaPoupanca = 4000;
+        double valorPlaystation = 5000F;
+
+        double cashTotal = valueTotalCountBank + valorTotalContaPoupanca;
+
+        boolean isPlaystation5Compravel = cashTotal >= valorPlaystation || valueTotalCountBank >= valorPlaystation || valorTotalContaPoupanca >= valorPlaystation;
+
+        System.out.println("\nDá pra comprar o PS5?");
+        System.out.println("Valor: " + valorPlaystation);
+        System.out.println("Total de dinheiro na conta: R$" + cashTotal);
+        if (isPlaystation5Compravel) {
+            System.out.println("Dá pra comprar grl");
+        } else {
+            System.out.println("Ih dá não :(");
+        }
+
+        // = += -= *= /= %=
+        double bonus = 0;
+        bonus += 500;
+        System.out.println(bonus);
+        bonus -= 400;
+        System.out.println(bonus);
+
     }
 }
