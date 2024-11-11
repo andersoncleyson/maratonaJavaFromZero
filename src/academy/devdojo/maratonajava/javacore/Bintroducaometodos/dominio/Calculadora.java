74d475b7b4c58867777dd00bce3b8db93a63c2d8
unknown <andersoncleyson@protonmail.com>
2024-11-11 17:47:54 -0300

    Métodos pt2

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -32,4 +32,24 @@ public class Calculadora {
         System.out.println("Num1 " + num1);
         System.out.println("Num2 " + num2);
     }
+
+    public void somaArray(int[] numeros){
+        int soma = 0;
+
+        for(int i: numeros ){
+            soma += i;
+        }
+
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs(int... numeros){
+        int soma = 0;
+
+        for(int i: numeros ){
+            soma += i;
+        }
+
+        System.out.println(soma);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -1,11 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressoraEstudante = new ImpressoraEstudante();
 
         estudante01.nome = "Shayla";
         estudante01.idade = 16;
@@ -15,12 +17,9 @@ public class EstudanteTest01 {
         estudante02.idade = 17;
         estudante02.sexo = 'M';
 
-        System.out.println(estudante01.nome);
-        System.out.println(estudante01.idade);
-        System.out.println(estudante01.sexo);
+        impressoraEstudante.imprime(estudante01);
+        impressoraEstudante.imprime(estudante02);
 
-        System.out.println(estudante02.nome);
-        System.out.println(estudante02.idade);
-        System.out.println(estudante02.sexo);
+        estudante01.imprime();
     }
 }
