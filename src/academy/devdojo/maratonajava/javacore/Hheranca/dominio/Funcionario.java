00434d5c30bad476f5eac3b4ae4339cd6064a333
Elliot Anderson <andersoncleyson@protonmail.com>
2024-11-25 01:33:43 -0300

    Herança

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
+
+public class Funcionario extends Pessoa{
+    private double salario;
+
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento(){
+        System.out.println("Eu "+this.getNome()+" recebi " + this.salario);
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 9");
+        endereco.setCep("2342342");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Elliot");
+        pessoa.setCpf("5959595");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("5465465");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+
+        funcionario.imprime();
+
+
+    }
+}
