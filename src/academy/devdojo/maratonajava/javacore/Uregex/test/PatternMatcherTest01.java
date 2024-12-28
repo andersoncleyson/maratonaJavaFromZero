cc371152fb07606af6a82c6d466bbefd22f7f854
Elliot Anderson <andersoncleyson@protonmail.com>
2024-12-27 22:10:08 -0300

    Regex pt2

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest01.java
@@ -18,3 +18,4 @@ public class PatternMatcherTest01 {
         }
     }
 }
+
--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -3,18 +3,25 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
-        String regex = "ab";
-        String texto = "abaaba";
+        // \d = Todos os digitos
+        // \D = Tudo o que não for digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracters excluíndo os brancos
+        // \w = a-ZA-Z, digitos, _
+        // \W = Tudo o que não for incluso no \w
+        String regex = "\\w";
+        //String texto = "abaaba";
+        String texto2 = "@#hhj2 1\t2gvh21";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(texto);
-        System.out.println("texto: "+texto);
+        Matcher matcher = pattern.matcher(texto2);
+        System.out.println("texto: "+texto2);
         System.out.println("indice: 0123456789");
         System.out.println("regex "+regex);
         System.out.println("Posições encontradas");
         while (matcher.find()){
-            System.out.println(matcher.start()+" ");
+            System.out.println(matcher.start()+" "+matcher.group()+"\n");
         }
     }
 }
