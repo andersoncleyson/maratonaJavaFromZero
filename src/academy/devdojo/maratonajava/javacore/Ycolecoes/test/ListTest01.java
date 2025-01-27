33a23a82356b5e881ace402d93bc23651676d99a
elliot <andersoncleyson@protonmail.com>
2025-01-27 17:48:27 -0300

    Listas pt1

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>(16); // 1.4
+        nomes.add("Elliot");
+        nomes.add("DevDojo Academy");
+
+        for (Object nome: nomes) {
+            System.out.println(nome);
+        }
+
+        nomes.add("Shayla");
+        System.out.println("---------");
+        for(int i = 0; i < nomes.size(); i++){
+            System.out.println(nomes.get(i));
+        }
+    }
+}
