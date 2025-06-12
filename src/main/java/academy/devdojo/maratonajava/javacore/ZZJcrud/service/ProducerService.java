e7f2a700daeb091e68767ae1bb4fac24d20eebb6
Elliot Anderson <andersoncleyson@protonmail.com>
2025-06-12 19:15:30 -0300

    JDBC - CRUD - delete

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZJcrud/service/ProducerService.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZJcrud/service/ProducerService.java
@@ -6,13 +6,17 @@ import academy.devdojo.maratonajava.javacore.ZZJcrud.repository.ProducerReposito
 import java.util.List;
 import java.util.Scanner;
 
-public class ProducerService {
-    private static Scanner scanner = new Scanner(System.in);
 
-    public static void buildMenu(int op){
+public class ProducerService {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void menu(int op){
         switch (op){
             case 1:
                 findByName();
+                break;
+            case 2:
+                delete();
                 break;
             default:
                 throw new IllegalArgumentException("Not a valid option");
@@ -21,11 +25,22 @@ public class ProducerService {
 
     private static void findByName(){
         System.out.println("Type the name or empty to all");
-        String name = scanner.nextLine();
+        String name = SCANNER.nextLine();
         List<Producer> producers = ProducerRepository.findByName(name);
         for (int i = 0; i < producers.size(); i++){
-            producers.get(i);
-            System.out.printf("[%d] - %s%n", i, producers.get(i).getName());
+            Producer producer = producers.get(i);
+            System.out.printf("[%d] - ID %d %s%n", i, producer.getId(), producer.getName());
         }
     }
+
+    private static void delete(){
+        System.out.println("Type one of the ids below to delete");
+        int id = Integer.parseInt(SCANNER.nextLine());
+        System.out.println("Are you sure? S/N");
+        String choice = SCANNER.nextLine();
+        if("s".equalsIgnoreCase(choice)){
+            ProducerRepository.delete(id);
+        }
+    }
+
 }
